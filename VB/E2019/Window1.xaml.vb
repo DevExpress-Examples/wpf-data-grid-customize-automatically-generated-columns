f9f0d23ced8824f1c77248b8d343acb3ad97902a
DevExpressExampleBot <fakeEmail>
2022-03-30 16:42:57 +0300

    Source auto update [skip ci]

--- VB/E2019/Window1.xaml.vb
+++ VB/E2019/Window1.xaml.vb
@@ -1,20 +1,21 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports System.Windows
-Imports System.Windows.Documents
 Imports DevExpress.Xpf.Grid
 Imports System.Windows.Data
 Imports System.Windows.Media
 
 Namespace E2019
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            grid.ItemsSource = IssueList.GetData()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = IssueList.GetData()
         End Sub
+
         Private Sub OnColumnsGenerated(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            For Each column As GridColumn In grid.Columns
+            For Each column As GridColumn In Me.grid.Columns
                 Select Case column.FieldName
                     Case "IssueName"
                         column.CellTemplate = TryCast(Application.Current.MainWindow.Resources("IssueNameTemplate"), DataTemplate)
@@ -24,11 +25,13 @@ Namespace E2019
                     Case "ID"
                         column.Visible = False
                 End Select
-            Next column
+            Next
         End Sub
+
         Public Class IssueList
-            Public Shared Function GetData() As List(Of IssueDataObject)
-                Dim data As New List(Of IssueDataObject)()
+
+            Shared Public Function GetData() As List(Of IssueDataObject)
+                Dim data As List(Of IssueDataObject) = New List(Of IssueDataObject)()
                 data.Add(New IssueDataObject() With {.ID = 0, .IssueName = "Transaction History", .IssueType = "Bug", .IsPrivate = True})
                 data.Add(New IssueDataObject() With {.ID = 1, .IssueName = "Ledger: Inconsistency", .IssueType = "Bug", .IsPrivate = False})
                 data.Add(New IssueDataObject() With {.ID = 2, .IssueName = "Data Import", .IssueType = "Request", .IsPrivate = False})
@@ -36,11 +39,16 @@ Namespace E2019
                 Return data
             End Function
         End Class
+
         Public Class IssueDataObject
-            Public Property ID() As Integer
-            Public Property IssueName() As String
-            Public Property IssueType() As String
-            Public Property IsPrivate() As Boolean
+
+            Public Property ID As Integer
+
+            Public Property IssueName As String
+
+            Public Property IssueType As String
+
+            Public Property IsPrivate As Boolean
         End Class
     End Class
 
@@ -48,15 +56,9 @@ Namespace E2019
         Implements IValueConverter
 
         Public Function Convert(ByVal value As Object, ByVal targetType As System.Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-            If value Is Nothing Then
-                Return Nothing
-            End If
-
+            If value Is Nothing Then Return Nothing
             Dim issueType As String = value.ToString()
-            If issueType = "Bug" Then
-                Return New SolidColorBrush(Colors.Red)
-            End If
-
+            If Equals(issueType, "Bug") Then Return New SolidColorBrush(Colors.Red)
             Return New SolidColorBrush(Colors.Black)
         End Function
 
@@ -64,5 +66,4 @@ Namespace E2019
             Throw New System.NotImplementedException()
         End Function
     End Class
-
 End Namespace
